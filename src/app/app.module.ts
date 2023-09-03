229b8ea3d315eb456ee54428cd444b593402bc00
nanokozmos <ovuncuzun@gmail.com>
2023-09-03 23:13:31 +0100

    adding calendar view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,15 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ViewChild, TemplateRef } from '@angular/core';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
 import { CalendarService } from 'src/app/services/calendar.service';
 
 @Component({
@@ -8,10 +12,23 @@ import { CalendarService } from 'src/app/services/calendar.service';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent {
+  @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any> | undefined;
   subscription: Subscription | undefined;
   calendarData: any;
+  CalendarView = CalendarView;
+  view: CalendarView = CalendarView.Week;
+  viewDate: Date = new Date('2023-07-09');
 
-  constructor(private calendarService: CalendarService) {
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  } | undefined;
+
+  refresh = new Subject<void>();
+  events: CalendarEvent[] = [];
+  activeDayIsOpen: boolean = false;
+
+  constructor(private calendarService: CalendarService, private modal: NgbModal) {
   }
 
   ngOnInit() {
@@ -21,7 +38,35 @@ export class CalendarComponent {
   getTimeTableData() {
     this.subscription = this.calendarService.getTimeTableData().subscribe(data => {
       this.calendarData = data;
+      this.addTimeTableDataEvents();
+      this.refresh.next();
     })
   }
 
+  addTimeTableDataEvents() {
+    this.calendarData.map((event: any) => (
+      this.events.push({
+        title: event.display_value.toString(),
+        start: new Date(event.date_time),
+        draggable: false,
+        resizable: {
+          beforeStart: false,
+          afterEnd: false,
+        }
+      })
+    ));
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
 }
